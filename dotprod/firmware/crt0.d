1e9666ce41df8d1ed69c46226a82c8ddaa04a443
JoaoEmanuelCandido <joaoemanu3lcandido@gmail.com>
2025-10-02 10:28:09 -0300

    Correção de Tamanho de arquivos

--- dotprod/firmware/crt0.d
+++
@@ -1,2 +0,0 @@
-crt0.o: \
- /home/joao/AppsUserWork/LiteX/litex-setup/litex/litex/soc/cores/cpu/vexriscv/crt0.S
--- dotprod/litex/build/colorlight_i5/software/bios/boot-helper.d
+++
@@ -1,2 +0,0 @@
-boot-helper.o: \
- /home/joao/AppsUserWork/LiteX/litex-setup/litex/litex/soc/cores/cpu/vexriscv/boot-helper.S
